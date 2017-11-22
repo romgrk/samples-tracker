71ebb8babcb8ed15800857c768fa9bb7b337fc6a
Rom Grk <romgrk.cc@gmail.com>
2017-11-22 18:11:14 -0500

    add Samples component

--- client/src/actions/samples.js
+++ client/src/actions/samples.js
@@ -5,16 +5,16 @@ import { createFetchActions } from '../utils/create-actions'
 import * as requests from '../requests'
 
 const samples = {
-  fetch:  createFetchActions(TEMPLATES.FETCH,  requests.samples.list),
-  update: createFetchActions(TEMPLATES.UPDATE, requests.samples.update,
+  fetch:  createFetchActions(SAMPLES.FETCH,  requests.samples.list),
+  update: createFetchActions(SAMPLES.UPDATE, requests.samples.update,
     (id, data) => ({ id, data }),
     (res, id, data) => ({ id, data }),
     (err, id, data) => ({ id, data })),
-  create: createFetchActions(TEMPLATES.CREATE, requests.samples.create,
+  create: createFetchActions(SAMPLES.CREATE, requests.samples.create,
     (id, data) => ({ id, data }),
     (res, id, data) => ({ id, data }),
     (err, id, data) => ({ id, data })),
-  delete: createFetchActions(TEMPLATES.DELETE, requests.samples.delete,
+  delete: createFetchActions(SAMPLES.DELETE, requests.samples.delete,
     undefined,
     (res, id, data) => ({ id, data }),
     (err, id, data) => ({ id, data })),
---
+++ client/src/components/Samples.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import pure from 'recompose/pure'
+import styled from 'styled-components'
+
+import Button from './Button'
+import Input from './Input'
+import Label from './Label'
+import Spinner from './Spinner'
+import EditableLabel from './EditableLabel'
+
+
+class Samples extends React.Component {
+  constructor(props) {
+    super()
+
+    const { data, data: { name, steps } } = props
+
+    this.state = {
+      data: {
+        ...data,
+        name: name || '',
+        steps: steps || [],
+      },
+    }
+  }
+
+  componentWillReceiveProps(props) {
+    const { data } = props
+    this.setState({ data })
+  }
+
+  setName = name => {
+    const data = { ...this.state.data, name }
+    this.setState({ data })
+    this.props.onChange(data.id, data)
+  }
+
+  render() {
+    const { isLoading, onChange, onCreate, onError } = this.props
+    const { editing } = this.state
+    const { name, steps } = this.state.data
+
+    return (
+      <div className='Samples'>
+        <div className='Template__info vcenter'>
+          <EditableLabel onEnter={this.setName} value={name} />
+          {
+            isLoading &&
+              <Spinner />
+          }
+        </div>
+        <div className='Steps'>
+          {
+            steps.map(step =>
+              <div className='Step center'>
+                <Label small>{ step.name }</Label>
+              </div>
+            )
+          }
+        </div>
+      </div>
+    )
+  }
+}
+
+export default pure(Samples)
