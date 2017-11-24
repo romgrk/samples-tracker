b7c82cc7a6eefda8231ae2c2a7c0622fd909256f
Rom Grk <romgrk.cc@gmail.com>
2017-11-24 13:10:54 -0500

    add StatusIcon

---
+++ client/src/components/StatusIcon.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import pure from 'recompose/pure'
+
+import STATUS from '../constants/status'
+import Icon from './Icon'
+
+const StatusIcon = ({ name }) =>
+  name === STATUS.NOT_DONE    ?  <Icon name='circle' subtle /> :
+  name === STATUS.IN_PROGRESS ?  <Icon name='circle' info /> :
+  name === STATUS.DONE        ?  <Icon name='check' success /> :
+  name === STATUS.ERROR       ?  <Icon name='warning' error /> :
+  name === STATUS.ON_HOLD     ?  <Icon name='hourglass-2' warning /> : undefined
+
+export default pure(StatusIcon)
