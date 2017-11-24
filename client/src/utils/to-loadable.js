b6b680655925a536e50c38967dae87b720ac6322
Rom Grk <romgrk.cc@gmail.com>
2017-11-24 13:11:41 -0500

    implement fromLoadable

--- client/src/utils/to-loadable.js
+++ client/src/utils/to-loadable.js
@@ -14,3 +14,15 @@ export default function toLoadable(object) {
   })
   return result
 }
+
+export function fromLoadable(object) {
+  if (Array.isArray(object)) {
+    return object.map(v => v.data)
+  }
+
+  const result = {}
+  Object.keys(object).forEach(key => {
+    result[key] = object[key].data
+  })
+  return result
+}
