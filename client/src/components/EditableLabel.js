2c4a660aed4e5db8a1fdb71c079903dad9e80b66
Rom Grk <romgrk.cc@gmail.com>
2017-11-29 14:11:44 -0500

    adjust EditableLabel

--- client/src/components/EditableLabel.js
+++ client/src/components/EditableLabel.js
@@ -107,11 +107,4 @@ class EditableLabel extends React.Component {
   }
 }
 
-function has(props, name) {
-  if (((name in props) && props[name] === undefined) || props[name] === true)
-    return true
-  return false
-}
-
-
 export default EditableLabel
