5e5b56517c75cb679f80977f46382975c4e12f05
Rom Grk <romgrk.cc@gmail.com>
2017-11-30 14:16:48 -0500

    <Help/>

---
+++ client/src/components/Help.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import pure from 'recompose/pure'
+import classname from 'classname'
+
+import Icon from './Icon'
+import Tooltip from './Tooltip'
+
+function Help(props) {
+  const {
+    children,
+    className,
+    ...rest
+  } = props
+
+  const helpClassName = classname(
+    'Help',
+    className
+  )
+
+  return (
+    <Tooltip content={children} offset='15px 0'>
+      <Icon className={helpClassName} name='question-circle' {...rest} />
+    </Tooltip>
+  )
+}
+
+export default pure(Help)
