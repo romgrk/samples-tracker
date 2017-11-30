be8ecbd8b26ea584df06fcdccadd8d5690c9b158
Rom Grk <romgrk.cc@gmail.com>
2017-11-30 12:44:14 -0500

    add icon-for-mime-type.js

---
+++ client/src/utils/icon-for-mime-type.js
@@ -0,0 +1,45 @@
+/*
+ * icon-for-mime-type.js
+ */
+
+export default function iconForMimeType(mime) {
+  if (mime.startsWith('image/'))
+    return 'file-image-o'
+
+  if (mime.startsWith('audio/'))
+    return 'file-audio-o'
+
+  if (mime.startsWith('video/'))
+    return 'file-video-o'
+
+  switch(mime) {
+    case 'application/pdf':
+      return 'file-pdf-o'
+
+    case 'application/zip':
+    case 'application/x-7z-compressed':
+    case 'application/x-bzip':
+    case 'application/x-bzip2':
+      return 'file-archive-o'
+
+    case 'text/html':
+    case 'text/javascript':
+    case 'application/json':
+    case 'application/xml':
+      return 'file-code-o'
+
+    case 'application/msword':
+      return 'file-word-o'
+
+    case 'application/vnd.ms-excel':
+    case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+      return 'file-excel-o'
+
+    default: break
+  }
+
+  if (mime.startsWith('text/'))
+    return 'file-text'
+
+  return 'file'
+}
