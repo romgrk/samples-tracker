fe67e1a127b52a58332464b0d48c493116d0c02d
Rom Grk <romgrk.cc@gmail.com>
2017-11-30 14:16:55 -0500

    <Link/>

---
+++ client/src/components/Link.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import pure from 'recompose/pure'
+import classname from 'classname'
+
+function Link(props) {
+  const {
+    children,
+    className,
+    small,
+    large,
+    info,
+    success,
+    warning,
+    error,
+    muted,
+    subtle,
+    highlight,
+    ...rest
+  } = props
+
+  const linkClassName = classname(
+    'link',
+    className,
+    {
+      'small': small,
+      'large': large,
+      'text-info': info,
+      'text-success': success,
+      'text-warning': warning,
+      'text-error': error,
+      'text-muted': muted,
+      'text-subtle': subtle,
+      'text-highlight': highlight,
+    }
+  )
+
+  return (
+    <a className={linkClassName} { ...rest }>
+      { children }
+    </a>
+  )
+}
+
+export default pure(Link)
