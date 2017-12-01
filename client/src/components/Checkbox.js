72ad07f6c0fb6b160c957e076e014c549b88f540
Rom Grk <romgrk.cc@gmail.com>
2017-12-01 12:21:58 -0500

    add Checkbox.js

---
+++ client/src/components/Checkbox.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import pure from 'recompose/pure'
+import classname from 'classname'
+
+class Checkbox extends React.Component {
+
+  onChange = (ev) => {
+    this.props.onChange && this.props.onChange(ev.target.checked, ev)
+  }
+
+  render() {
+    const {
+      children,
+      className,
+      small,
+      large,
+      info,
+      success,
+      warning,
+      error,
+      muted,
+      subtle,
+      highlight,
+      ...rest
+    } = this.props
+
+    const labelClassName = classname(
+      'input-label',
+      className,
+      {
+        'small': small,
+        'large': large,
+        'text-info': info,
+        'text-success': success,
+        'text-warning': warning,
+        'text-error': error,
+        'text-muted': muted,
+        'text-subtle': subtle,
+        'text-highlight': highlight,
+      }
+    )
+
+    return (
+      <label className={labelClassName}>
+        <input type='checkbox' className='Checkbox' { ...rest } /> { children }
+      </label>
+    )
+  }
+}
+
+export default pure(Checkbox)
