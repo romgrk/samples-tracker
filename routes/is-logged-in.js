bfb2090c9d7bc5d67cec563f3f1679799817a0a9
Rom Grk <romgrk.cc@gmail.com>
2017-12-04 16:01:08 -0500

    is-logged-in: resolve to false

--- routes/is-logged-in.js
+++ routes/is-logged-in.js
@@ -2,14 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 const { dataHandler, errorHandler } = require('../helpers/handlers.js')
-const { rejectMessage } = require('../helpers/promise.js')
 
 /* GET check if user is logged in */
 router.use('/', (req, res, next) => {
   (
     req.isAuthenticated() ?
-      Promise.resolve() :
-      rejectMessage('Not logged in')
+      Promise.resolve(true) :
+      Promise.resolve(false)
   )
   .then(dataHandler(res))
   .catch(errorHandler(res))
