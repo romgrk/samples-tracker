bf0d1d38e37e7cc8d8e0b77caa7c8638b029b765
Rom Grk <romgrk.cc@gmail.com>
2017-12-04 16:47:58 -0500

    comment

--- client/src/utils/open-centered.js
+++ client/src/utils/open-centered.js
@@ -1,4 +1,8 @@
-export default function openCentered(url, height, width) => {
+/*
+ * open-centered.js
+ */
+
+export default function openCentered(url, height, width) {
   const left = window.screen.width / 2  - width / 2
   const top  = window.screen.height / 2 - height / 2
   return window.open(url, '_blank', `height=${height},width=${width},left=${left},top=${top}`)
