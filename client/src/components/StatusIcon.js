9a1dad46f6d3b1f5a26537f4883f873335f1b021
Rom Grk <romgrk.cc@gmail.com>
2017-12-08 12:30:03 -0500

    display spinner on status icon loading

--- client/src/components/StatusIcon.js
+++ client/src/components/StatusIcon.js
@@ -3,9 +3,10 @@ import pure from 'recompose/pure'
 
 import STATUS from '../constants/status'
 import Icon from './Icon'
+import Spinner from './Spinner'
 
 const StatusIcon = ({ name, showInProgress, size }) =>
-  name.isLoading              ?  <Icon name='circle-o-notch' info spin size={size} /> :
+  name.isLoading              ?  <Spinner /> :
   name === STATUS.NOT_DONE    ?  <Icon name='circle' subtle size={size} /> :
   name === STATUS.IN_PROGRESS ?  (showInProgress ?
                                     <Icon name='circle-o-notch' info size={size} /> :
