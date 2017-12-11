0389412ea200be599dcb4c16150faa2eed93ac72
Rom Grk <romgrk.cc@gmail.com>
2017-12-11 12:48:16 -0500

    complete history descriptions

--- helpers/generate-history-entry.js
+++ helpers/generate-history-entry.js
@@ -7,19 +7,60 @@ const { diff } = require('deep-diff')
 module.exports = function generateHistoryEntry(user, a, b) {
 
   let differences = diff(a, b)
-  console.log(JSON.stringify(a))
-  console.log(JSON.stringify(b))
 
   differences = differences.filter(d => {
     return (d.path[0] !== 'history')
   })
 
-  console.log(differences)
+  let difference = differences[0]
+  let property = difference.path[0]
+
+  let description
+  let stepIndex = null
+
+  if (property === 'notes') {
+    if (difference.rhs === '')
+      description = 'deleted notes'
+    else
+      description = 'modified notes'
+  }
+  else if (property === 'tags') {
+
+    if (difference.kind === 'A') {
+      if (difference.item.kind === 'N') {
+        description = `added tag “${difference.item.rhs}”`
+      }
+      else if (difference.item.kind === 'D') {
+        description = `deleted tag “${difference.item.lhs}”`
+      }
+    }
+    else /* kind === E */ {
+      description = `deleted tag “${difference.lhs}”`
+    }
+  }
+  else if (property === 'steps') {
+    stepIndex = difference.path[1]
+
+    const stepProperty = difference.path[2]
+
+    if (stepProperty === 'notes') {
+      if (difference.rhs === '')
+        description = 'deleted step notes'
+      else
+        description = 'modified step notes'
+    }
+    else {
+      description = `set “${difference.path[2]}” to “${difference.rhs}”`
+    }
+  }
+  else {
+    description = 'updated sample'
+  }
 
   return {
     sampleId: a.id,
-    stepIndex: null,
+    stepIndex: stepIndex,
     userId: user.id,
-    description: ''
+    description: description
   }
 }
