d135713995582001c765f95cc7e5e7619b3c5778
Rom Grk <romgrk.cc@gmail.com>
2017-12-12 12:55:50 -0500

    add external sample update route

--- routes/sample.js
+++ routes/sample.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const generateHistoryEntry = require('../helpers/generate-history-entry')
-const { dataHandler, errorHandler } = require('../helpers/handlers.js')
+const { okHandler, dataHandler, errorHandler } = require('../helpers/handlers.js')
+const parseForm = require('../helpers/parse-form')
 const Sample = require('../models/sample.js')
+const File = require('../models/file.js')
 const History = require('../models/history.js')
 
 /* GET samples list */
@@ -60,7 +62,7 @@ router.use('/update/:id', (req, res, next) => {
   .catch(errorHandler(res))
 })
 
-/* POST complete sample step */
+/* POST update step status */
 router.use('/update-step-status/:id/:index/:status', (req, res, next) => {
   const sampleId = req.params.id
   const stepIndex = Number(req.params.index)
@@ -84,6 +86,60 @@ router.use('/update-step-status/:id/:index/:status', (req, res, next) => {
 router.use('/delete/:id', (req, res, next) => {
   Sample.delete(req.params.id)
   .then(dataHandler(res))
+  .catch(errorHandler(res))
+})
+
+
+/*
+ * POST update step status/state [EXTERNAL USAGE]
+ * This route is to be used by external software to update
+ * a sample/step status, notes, and files
+ */
+router.use('/update-step/:id/:index', (req, res, next) => {
+  const sampleId = req.params.id
+  const stepIndex = Number(req.params.index)
+
+  parseForm(req),
+  .then(({ fields: { status, notes }, files }) => {
+
+    const actions = []
+
+    actions.push(Sample.updateStepStatus(sampleId, stepIndex, status, req.user)
+    .then(sample => {
+      History.create({
+        sampleId: sampleId,
+        stepIndex: stepIndex,
+        userId: req.user.id,
+        description: `changed status to ${status}`
+      })
+      return sample
+    })
+    .then(sample => {
+      sample.notes += notes
+      return Sample.update(sample)
+    }))
+
+    actions.push(Object.values(files).map(file =>
+      File.create({
+        sampleId,
+        stepIndex,
+        name: file.name,
+        mime: file.type,
+      }, file.path)
+      .then(file => {
+        History.create({
+          sampleId: sampleId,
+          stepIndex: stepIndex,
+          userId: req.user.id,
+          description: `added file ${file.name}`
+        })
+        return file
+      })
+    ))
+
+    return Promise.all(actions)
+  })
+  .then(okHandler(res))
   .catch(errorHandler(res))
 })
 
